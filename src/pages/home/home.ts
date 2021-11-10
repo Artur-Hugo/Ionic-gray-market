f43521445d055754ef32b99bc02a31b677f258fd
Artur-Hugo <Artur.Hugoliga@gmail.com>
2021-11-10 14:21:17 -0300

    Desabilitar menu na tela inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController } from 'ionic-angular';
+import { IonicPage, MenuController, NavController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -8,11 +8,20 @@ import { IonicPage, NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public menu: MenuController) {
 
   }
 
+  //Função para deixar desabilitado e habilitar o menu quando sai da tela de login
+  ionViewWillEnter() {
+    this.menu.swipeEnable(false);
+    }
+    ionViewDidLeave() {
+    this.menu.swipeEnable(true);
+    }
+
   login(){
+    //Navegar para a proxima tela
     this.navCtrl.setRoot('CategoriasPage');
   }
 
